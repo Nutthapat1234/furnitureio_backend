26499a86e5cf96755766be3c34564ac39be75bf9
Inter_Nutthapat <nutp1999@gmail.com>
2019-11-25 18:23:11 +0700

    add Static Root

--- furnitureIO/settings.py
+++ furnitureIO/settings.py
@@ -121,15 +121,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 MEDIA_URL = "/media/"
 
 URL = 'http://127.0.0.1:8000/'
-
-PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
