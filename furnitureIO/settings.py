3f9b20e0e249c3979dca84560dd27b04d68d99c7
Inter_Nutthapat <nutp1999@gmail.com>
2019-11-25 16:26:54 +0700

    fix charset error

--- furnitureIO/settings.py
+++ furnitureIO/settings.py
@@ -80,9 +80,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'furnitureIO',
-        'OPTIONS': {
-            # Tell MySQLdb to connect with 'utf8mb4' character set
-            'charset': 'utf8mb4'},
         'USER': 'root',
         'PASSWORD': 'admin',
         'HOST': '127.0.0.1',  # Or an IP Address that your DB is hosted on
